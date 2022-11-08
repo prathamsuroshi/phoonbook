14d54ea41937be40930a8572591abcc68bd76a3a
prathamsuroshi <psuroshi9@gmail.com>
2022-11-09 01:35:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import data from "./phone-book-data.json";
 
-function App() {
+import {nanoid} from "nanoid";
+
+const App = () => {
+  const [contacts , setcontact] = useState(data);
+
+  const [adddata , addnewdata] = useState({
+    fullName:"",
+    address:"",
+    phoneNumber:"",
+    email:"",
+  });
+
+  const handleaddformchange = (event) =>{
+    event.preventDefault();
+
+    const fieldname = event.target.getAttribute("name");
+    const fieldvalue = event.target.value;
+
+    const newformData = {...adddata};
+     newformData[fieldname] = fieldvalue; 
+
+     addnewdata(newformData);
+  };
+
+  const handleaddfromsubmit = (event) =>{
+    event.preventDefault();
+
+    const newdata = {
+      id : nanoid(),
+      fullName: adddata.fullName,
+      email: adddata.email,
+      address : adddata.address,
+      phoneNumber : adddata.phoneNumber,
+    };
+
+    const newcontact = [...contacts,newdata];
+    setcontact(newcontact);
+
+  }
+  
+  const handelDeleteclick = (contactId) => {
+    const newcontact = [...contacts];
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+
+    newcontact.splice(index, 1);
+    setcontact(newcontact);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='container'>
+      <table>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Phone Number</td>
+            <td>Address</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) =>
+          <tr>
+          <td>{contact.fullName}</td>
+          <td>{contact.email}</td>
+          <td>{contact.phoneNumber}</td>
+          <td>{contact.address}</td>
+          <td><button onClick={() => handelDeleteclick(contact.id)}>delete</button></td>
+        </tr> 
+          )}
+        </tbody>
+      </table>
+
+      <form onSubmit={handleaddfromsubmit}>
+        <div className='addcontact'>
+          <h2>Add new Contact</h2>
+          <input name='fullName' type="text" required="required" placeholder="Enter your name" onChange={handleaddformchange} ></input>
+          <input name='email' type="text" required="required" placeholder="Enter your email" onChange={handleaddformchange}></input>
+          <input name='phoneNumber' type="text" required="required" placeholder="Enter your phone Number" onChange={handleaddformchange}></input>
+          <input name='address' type="text" required="required" placeholder="Enter your address" onChange={handleaddformchange}></input>
+          <button type='submit' >Add</button>
+        </div>
+      </form>
     </div>
-  );
+    
+  )
 }
 
-export default App;
+export default App
